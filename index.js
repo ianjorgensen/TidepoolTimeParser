ca5c0d8f8788b9d69ed34a779c3ed35f0deb6f89
ianjorgensen <jorgensen.ian@gmail.com>
2013-10-02 13:59:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var moment = require('moment');
+
+var valid = function(rawTime) {
+  if(moment(rawTime).toISOString().indexOf('NaN') == -1) {
+    return true;
+  }
+  return false;
+};
+
+var getCreatedTime = function(rawTime) {
+  var time = moment(rawTime);
+
+  return {
+    raw: rawTime,
+    unix: time.format('X'),
+    unixMili: time.format('X') * 1000,
+    daytime: time.format('h:mm a ddd Do'),
+    time: time.format('h:mm a'),
+    day: time.format('D'),
+    month: time.format('M'),
+    year: time.format('YYYY'),
+    format: {
+      day: time.format('dddd[,] MMMM Do[,]YYYY')
+    },
+    dayMilliseconds: (time.format('X')*1000 - time.startOf('day').format('X')*1000),
+    startOfDay: time.startOf('day').format('X')*1000,
+    daysSinceEpox: (time.startOf('day').format('X')*1000 - moment(0).startOf('day').format('X')*1000) / (1000*60*60*24)
+  }
+};
+
+module.exports = {
+	getCreatedTime: getCreatedTime,
+  valid: valid
+};
